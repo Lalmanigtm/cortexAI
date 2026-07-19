6a0d07489df68c8ff17a9c878571b43c33cbadce
Saurab Gautam <missionprojects250@gmail.com>
2026-07-19 18:43:42 +0545

    01_connect to mongoDB database

--- backend/services/auth/index.ts
+++ backend/services/auth/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db";
 dotenv.config();
 
 const port = process.env.PORT;
@@ -12,4 +13,5 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
     console.log(`auth start at ${port}`);
+    connectDB()
 });
