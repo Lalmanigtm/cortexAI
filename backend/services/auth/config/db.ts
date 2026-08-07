0953b1766538b54f29cb9668e01317b73048335e
Saurab Gautam <missionprojects250@gmail.com>
2026-08-07 08:04:48 +0545

    10-setuping the reduired db.ts inside services

--- backend/services/auth/config/db.ts
+++ backend/services/auth/config/db.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URL)
+        await mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost:27017/mern-auth")
         console.log("db connect")
     } catch (error) {
         console.log(`db error ${error}`)
